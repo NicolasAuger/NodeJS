6ed771d7e665411bf41c7b83fac96caba8e2fc29
Nicolas AUGER <Nicolas AUGER>
2016-10-11 00:29:30 +0200

    FileSystem + Suite Main + Crea BDD

--- tp/index.js
+++ tp/index.js
@@ -7,13 +7,14 @@ const rl = require('readline').createInterface({
 var request = require('request')
 var api = require('marvel-api')
 var jsonfile = require('jsonfile')
+const lowerCase = require('lower-case')
 const fs = require('fs')
 const _ = require('lodash')
 const db = require('sqlite')
 const inquirer = require('inquirer')
 var program = require('commander')
 
-db.open(':memory:').then((res) => {
+db.open('marvel.db').then((res) => {
   db.run("CREATE TABLE IF NOT EXISTS marvel (heroes)")
 })
 
@@ -26,6 +27,7 @@ program
   .version('1.0')
   .option('-c, --characters', 'Liste des 20 premiers Heros de Marvel')
   .option('-m, --main', 'Demarre le programme Main')
+  .option('-d, --database', 'Affiche ce qui se trouve dans la base de donnee')
   .parse(process.argv)
 
 
@@ -39,6 +41,10 @@ if(program.characters) {
   })
   .fail(console.error)
   .done()
+}else if (program.database) {
+  db.each("SELECT * FROM marvel", function(err, row) {
+    console.log(row.heroes)
+  })
 }else if (program.main) {
 
   var heroSearched = function(){
@@ -51,8 +57,23 @@ if(program.characters) {
       var question4 = "Dans quelles histoires apparait-il(elle) ?"
       var question5 = "Dans quels evenements apparait-il(elle)?"
 
+      // Je cherche dans l'api le hero par son nom
       marvel.characters.findByName(answer)
       .then(function(res) {
+
+        db.run("INSERT INTO marvel VALUES (?)", [res.data[0].name])
+        try {
+          //On ecrit dans un fichier l'ID, le nom, et la description du hero choisi
+          // On donne le nom de hero choisi comme nom de fichier.json
+          fs.writeFile(''+lowerCase(answer)+'.json', 'ID du Hero : ' + res.data[0].id + '\n\nNom du Hero : ' + res.data[0].name + '\n\nDescription du Hero : ' + res.data[0].description, (err) => {
+            if (err) throw err
+            console.log('Fichier ecrit')
+          })
+        }
+        catch (err) {
+          console.error('ERR > ', err)
+        }
+
         console.log('ID de votre Hero : ', res.data[0].id);
         inquirer.prompt([
           {
@@ -63,28 +84,41 @@ if(program.characters) {
               question1,
               question2,
               question3,
-              question4
+              question4,
+              question5
             ]
           }
         ]).then((answers) => {
           if (answers.hero == question1) {
-            console.log('Description du Hero : ', res.data[0].description)
+            console.log('\nDescription du Hero : ', res.data[0].description)
             return res.data[0].description
           }
           else if (answers.hero == question2) {
-            console.log('20 Comics dans lesquels le hero apparait : ', res.data[0].comics)
+            console.log("\n20 Comics dans lesquels le hero apparait : \n")
+            for (let i = 0 ; i < res.data[0].comics.items.length ; i++){
+              console.log(res.data[0].comics.items[i].name)
+            }
             return res.data[0].comics
           }
           else if (answers.hero == question3) {
-            console.log('20 Series dans lesquelles apparait le hero : ', res.data[0].series)
+            console.log("\n20 Series dans lesquelles le hero apparait : \n")
+            for (let i = 0 ; i < res.data[0].series.items.length ; i++){
+              console.log(res.data[0].series.items[i].name)
+            }
             return res.data[0].series
           }
           else if (answers.hero == question4) {
-            console.log('20 Histoires dans lesquelles se trouve le Hero : ', res.data[0].stories)
+            console.log("\n20 Stories dans lesquels le hero apparait : \n")
+            for (let i = 0 ; i < res.data[0].stories.items.length ; i++){
+              console.log(res.data[0].stories.items[i].name)
+            }
             return res.data[0].stories
           }
           else if (answers.hero == question5) {
-            console.log('20 Evenements dans lesquels le hero est present : ', res.data[0].events)
+            console.log("\n20 Events dans lesquels le hero apparait : \n")
+            for (let i = 0 ; i < res.data[0].events.items.length ; i++){
+              console.log(res.data[0].events.items[i].name)
+            }
             return res.data[0].events
           }
         })
